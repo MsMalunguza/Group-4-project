c8a5367f2665b1475e6c57f97ece293b802d44c0
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-05-22 00:38:32 +0200

    changing of colors intesity to moderate

--- src/pages/message.js
+++ src/pages/message.js
@@ -47,7 +47,7 @@ function Messaging() {
         />
         <button
           onClick={handleSendMessage}
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="px-4 py-2 font-bold text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-600"
         >
           Send
         </button>
