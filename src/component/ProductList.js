20edf2f6e0f6a6b87463aa397ea28aac9a535447
pachalomtipende <167616678+pachalomtipende@users.noreply.github.com>
2024-05-22 11:23:42 +0200

    adding scrolling effect to the pictures

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const ProductList = ({ items }) => {
     return (
-        <div className="product-list">
-            {items.map((item, index) => (
-                <div key={index} className="product-card">
-                    <div className="product-image-container">
-                        <img src={item.image} alt={item.name} className="product-image" />
+        <div className="product-list-container">
+            <div className="product-list">
+                {items.map((item, index) => (
+                    <div key={index} className="product-card">
+                        <div className="product-image-container">
+                            <img src={item.image} alt={item.name} className="product-image" />
+                        </div>
+                        <div className="product-info">
+                            <h3>{item.name}</h3>
+                            <p>{item.price}</p>
+                        </div>
                     </div>
-                    <div className="product-info">
-                        <h3>{item.name}</h3>
-                        <p>{item.price}</p>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
