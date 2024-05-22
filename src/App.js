3f2ad327cd0e07fe2e0a5141e4da71441b94b29e
MsMalunguza <167319794+MsMalunguza@users.noreply.github.com>
2024-05-22 17:55:07 +0200

    deleting draft committed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,4 @@
-import "./App.css";
-import React from "react";
 
-// import { NavLink } from "react-router-dom";
-
-// import "./App.css";
-// export const App = () => {
-//   return (
-//     <div className="flex flex col ww-full bg-red-500 items-start justify-start">
-//       <div className="flex flex-row h-10 w-full item-start justify start">
-//         Header
-//       </div>
-//       <div className="flex flex row w-full items h-auto bg grey-200 items start justify"></div>
-//       <div>
-//         <NavLink to="/home">Go to my home</NavLink>
-//         <div />
-//         <NavLink to="/login">log in </NavLink>
-//       </div>
-//       <div></div>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 import Register from "./Components/LogIn/Register";
 import LogIn from "./Components/LogIn/LogIn";
@@ -38,5 +15,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/Components/hostels.js
+++
@@ -1,6 +0,0 @@
-import React, { Fragment } from "react";
-const hostels = () => {
-
-    return <Fragment>My log in page</Fragment>
-};
-export default hostels;
