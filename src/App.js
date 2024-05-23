4a62a96abf4a0896df3df61d265829334fb42736
MichaelMtonga <bsc-inf-14-21@unima.ac.mw>
2024-05-23 11:32:36 +0200

    first commit on my notification feature

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import "./App.css";
-import React from "react";
-
-// import { NavLink } from "react-router-dom";
-
-// import "./App.css";
-// export const App = () => {
-//   return (
-//     <div className="flex flex col ww-full bg-red-500 items-start justify-start">
-//       <div className="flex flex-row h-10 w-full item-start justify start">
-//         Header
-//       </div>
-//       <div className="flex flex row w-full items h-auto bg grey-200 items start justify"></div>
-//       <div>
-//         <NavLink to="/home">Go to my home</NavLink>
-//         <div />
-//         <NavLink to="/login">log in </NavLink>
-//       </div>
-//       <div></div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import Register from "./Components/LogIn/Register";
-import LogIn from "./Components/LogIn/LogIn";
-import LogIn2 from "./Components/LogIn/LogIn2";
-import Register2 from "./Components/LogIn/Register2";
+import React from 'react';
+import NotificationBell from './Components/NotificationBell';
 
 function App() {
   return (
-    <div>
-      {/* <Register></Register> */}
-      {/* <LogIn></LogIn>*/}
-      {/*<LogIn2></LogIn2>*/}
-      <Register2></Register2> 
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <h1>Header</h1>
+        <NotificationBell />
+      </header>
+      <main className="flex-1 flex justify-center items-center">
+        <p></p>
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
     </div>
   );
 }
