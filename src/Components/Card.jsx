4bdaeaf6dba294a13e5ace6f03b88178498e5796
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-05-24 09:09:09 +0200

    Changes to Card.jsx were made

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,8 @@
 import img1 from "./laptop2.webp";
+import { NavLink } from "react-router-dom";
+import {
+  BsCart,
+} from "react-icons/bs";
 
 export default function Card() {
   return (
@@ -28,7 +32,16 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800"><button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button></button>
+            
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
@@ -56,7 +69,16 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800"><button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button></button>
+            
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
@@ -87,7 +109,15 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800"><button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button></button>
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
@@ -115,7 +145,15 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800"><button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button></button>
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
@@ -146,7 +184,15 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800"><button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button></button>
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
@@ -177,7 +223,15 @@ export default function Card() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <button class="font-semibold text-gray-800">Add to Basket</button>
+              <button class="font-semibold text-gray-800">
+            <NavLink to="/CartWrapper" className="flex items-center text-lg">
+              Add To Cart
+            </NavLink></button>
+            </span>
+            <span class="flex items-center justify-center mt-4 w-[50%] bg-yellow-400 hover:bg-yellow-500 py-1 rounded">
+              <button><NavLink to="/chatroom" className="text-white">
+                  Messaging
+                </NavLink></button>
             </span>
           </div>
         </div>
